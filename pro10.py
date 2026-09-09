8dae5f4e37b6c7a0924c8ff82a3a4de7c515a685
Muhammad Humayun <mhumyyunbaber@gmail.com>
2026-09-09 19:13:40 +0500

    Project>CNIC_instruction

---
+++ pro10.py
@@ -0,0 +1,7 @@
+Address=input("Enter your Address: ")
+Gender=input("Enter Your Gender: ")
+Age=int(input("Enter your Age:  "))
+if Age>=18:
+    print("Your now Eligible for Vote and Marriage")
+else:
+    print("NA, you're Not Eligible ")
