bb7c783235440bb1c279af3dd4e05926472640dd
Muhammad Humayun <mhumyyunbaber@gmail.com>
2026-09-11 16:26:06 +0500

    Login system

---
+++ pro11.py
@@ -0,0 +1,11 @@
+username="M_Humayun"
+password="mow24##cf"
+
+u=input("Enter Your Username:  ")
+p=input("Enter Your Password: ")
+
+if u==username and p==password:
+    print("Login succesfully")
+else:
+    print("Denied!!")
+    
